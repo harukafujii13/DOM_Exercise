1f711d23789b7ed8529e56e0e6fd1b6e8ba6ce00
Haruka <hfujii1013@gmail.com>
2023-01-27 18:05:10 -0800

    add edit and delete

--- DOM_exercise1/app.js
+++ DOM_exercise1/app.js
@@ -1,6 +1,6 @@
 //Adding
 const form = document.querySelector("#add");
-const input = document.querySelector("add-input");
+const addInputField = document.querySelector("add-input");
 const list = document.querySelector("#list");
 
 form.addEventListener("submit", function (evt) {
@@ -19,27 +19,103 @@ form.addEventListener("submit", function (evt) {
 
 
     //Solution2
-    const li = document.createElement("li");
-    const firstP = document.createElement("p");
-    const secondP = document.createElement("p");
-    const firstIcon = document.createElement("i");
-    const secondIcon = document.createElement("i");
-    const input = document.createElement("input");
+    // const li = document.createElement("li");
+    // const firstP = document.createElement("p");
+    // const secondP = document.createElement("p");
+    // const firstIcon = document.createElement("i");
+    // const secondIcon = document.createElement("i");
+    // const input = document.createElement("input");
 
-    firstIcon.className = "fa fa-pencil-square-o";
-    firstIcon.className = "fa fa-times";
-    input.className = "edit-note";
-    input.setAttribute("type", "text")
+    // firstIcon.className = "fa fa-pencil-square-o";
+    // firstIcon.className = "fa fa-times";
+    // input.className = "edit-note";
+    // input.setAttribute("type", "text")
 
-    firstP.textContent = inputValue;
+    // firstP.textContent = inputValue;
     
-    secondP.appendChild(firstIcon);
-    secondP.appendChild(secondIcon);
-    li.appendChild(firstP);
-    li.appendChild(secondP);
-    li.appendChild(input);
-    list.appendChild(li);
+    // secondP.appendChild(firstIcon);
+    // secondP.appendChild(secondIcon);
+    // li.appendChild(firstP);
+    // li.appendChild(secondP);
+    // li.appendChild(input);
+    // list.appendChild(li);
 
+    //Solution3
+    const clonedLi = document.querySelector("#list li").cloneNode(true)
+    clonedLi.firstElementChild.textContent = inputValue
+    list.appendChild(clonedLi)
 
-    evt.target[0].value = " ";
+    evt.target[0].value = "";
 });
+
+//Editing and Delrting
+list.addEventListener("click", function(e){
+    // console.log(this)
+    // console.log(e.target.classList);
+
+
+    //Click edit
+    if(e.target.classList[1] === "fa-pencil-spuare-o"){
+        let parentP = e.target.parentNode
+        console.dir(parentP);
+
+        //Clicked edit
+        parentP.style.display = "none"; //hide second p
+
+        let note = parentP.previousElementSibling
+        let editInput = parentP.nextElementSibling
+
+
+        editInput.style.display = "block"
+        editInput.value = note.textContent
+
+        editInput.addEventListener("keypress", function(e){
+            if(e.keyCode === 13){ //13 ---> decimal for carrige return
+                if(editInput.value !== ""){
+                    note.textContent = editInput.value
+                }
+                parentP.style.display = "block"
+                editInput.style.display = "none"
+            }
+        })
+
+    }
+    //Click deletes
+    if(e.target.classList[1] === "fa-times"){
+        const listToDelete = e.target.parentNode.parentNode
+        URL.removeChild(listToDelete) 
+
+    }
+})
+
+//Hide list
+const hideItem = document.querySelector("#hide")
+hideItem.addEventListener("click", function(e){
+    const label = document.querySelector("label")
+    if(this.checked){
+        label.textContent = "Unhide Note"
+        list.style.display = "none";
+    }else{
+        label.textContent = "Unhide Note"
+        list.style.display = "none";
+    }
+})
+
+// Serch Filter
+const searchInput = document.querySelector("#search-note input")
+serachInput.addEventListener("keyup", function(e){
+
+    const searchTerm = e.target.value.toLowerCase()
+    const noteList = list.getElementsByTagName("li")
+
+    // console.log(noteList);
+
+    Array.form(noteList).forEach(function(note){
+        const paraText = note.firstElementChild.textContent
+        if(paraText.toLowerCase().indexOf(serchInput) !== -1){
+            note.style.display = "block"
+        }else{
+            note.style.display = "none"
+        }
+    })
+})
---
+++ DOM_mario/MarioChallenge/app.js
@@ -0,0 +1,45 @@
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width
+	);
+}
+
+const init = () => {
+    //get the avatar
+    // const avatar = document.querySelector(".avatar")
+    //get the coin
+
+    moveCoin();
+    window.addEventListener('keyup', function(e){
+        if(e.key === 'ArrowDown' || e.key === 'Down'){
+            moveVertical(avatar, 50);
+        }
+
+        if(isTouching(avatar,coin)) moveCoin();
+    });
+}
+
+const moveVertical = (element, amount) => {
+    const currTop = extractPos(element.style.top);
+    element.style.top = `${currTop + amount}px`;
+}
+
+const extractPos = (position) => {
+    if(!position) return 100;
+    return parseInt(position.slice(0, -2))
+}
+
+const moveCoin = () => {
+    const x = Math.floor(Math.random() * window.innerWidth)
+    // const y = ?
+    coin.style.top = `${x}px`;
+    // coin.style.?? = ??
+}
+
+init();
